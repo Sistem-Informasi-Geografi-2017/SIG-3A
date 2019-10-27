63aa1ee98a93e0b5c389765c29d50f19bacee64c
Fahmifc123 <f4hm126@yahoo.co.id>
2019-10-28 00:02:19 +0700

    Fahmi tugas 2

---
+++ src/1174021/2/soal2.py
@@ -0,0 +1,15 @@
+import shapefile #meng-import library shapefile
+
+w = shapefile.Writer('soal2', shapeType=1) #membuat object Writer baru dan memberi argumen berupa nama file dari shapefilenya yaitu soal2 dan argumen berupa shapeTypenya yaitu 1 yang berupa point
+w.shapeType #menentukan shapeTypenya
+
+w.field("kolom1", "C") #membuat field baru dan memberi argumen berupa nama fieldnya yaitu kolom1 dan tipe datanya yaitu char
+w.field("kolom2", "C") #membuat field baru dan memberi argumen berupa nama fieldnya yaitu kolom2 dan tipe datanya yaitu char
+
+w.record("ngek", "satu") #mengisi data pada field yang telah dibuat yaitu kolom1 = ngek dan kolom2 = satu
+w.record("ngok", "dua") #mengisi data pada field yang telah dibuat yaitu kolom1 = ngok dan kolom2 = dua
+
+w.point(1,1) #mengisi data point sesuai koordinatnya yaitu x = 1 dan y = 1
+w.point(2,2) #mengisi data point sesuai koordinatnya yaitu x = 2 dan y = 2
+
+w.close() #menutup file
---
+++ src/1174021/2/soal6.py
@@ -0,0 +1,14 @@
+
+import shapefile #meng-import library shapefile
+
+w = shapefile.Writer('soal6', shapeType=shapefile.POLYGON) #membuat object Writer baru dan memberi argumen berupa nama file dari shapefilenya yaitu soal6 dan argumen berupa shapeTypenya yaitu shapefile.POLYGON yang berupa poligon
+w.shapeType #menentukan shapeTypenya
+
+w.field("kolom1", "C") #membuat field baru dan memberi argumen berupa nama fieldnya yaitu kolom1 dan tipe datanya yaitu char
+w.field("kolom2", "C") #membuat field baru dan memberi argumen berupa nama fieldnya yaitu kolom2 dan tipe datanya yaitu char
+
+w.record("ngek", "satu") #mengisi data pada field yang telah dibuat yaitu kolom1 = ngek dan kolom2 = satu
+
+w.poly([[[1,3],[5,3]]]) #mengisi data poly sesuai koordinatnya masing-masing
+
+w.close() #menutup file
